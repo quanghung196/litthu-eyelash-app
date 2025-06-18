8690f1f435e6ae6fc7b90947dad6bdcf5003c905
bqhung <thelonelydeath196@gmail.com>
2025-06-18 15:31:34 +0700

    Create base project v3: Done login screen

--- litthuApp/src/androidMain/kotlin/com/example/litthu_eyelash_app/utils/JsonManager.android.kt
+++
@@ -1,7 +0,0 @@
-package com.example.litthu_eyelash_app.utils
-
-actual fun readJsonFile(path: String): String {
-    return ClassLoader.getSystemResourceAsStream(path)?.use {
-        it.bufferedReader().readText()
-    } ?: throw IllegalStateException("Cannot read file: $path")
-}
--- litthuApp/src/commonMain/kotlin/com/example/litthu_eyelash_app/utils/JsonManager.kt
+++
@@ -1,3 +0,0 @@
-package com.example.litthu_eyelash_app.utils
-
-expect fun readJsonFile(path: String): String
